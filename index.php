dcdb37a93efbe833c501b705f1aa191091903b90
adzumii <93244765+adzumii@users.noreply.github.com>
2021-11-16 10:41:43 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<html> 
+<head> 
+<body bgcolor="purple"> 
+ 
+<title>Login paget</title> 
+</head> 
+<script type="text/javascript"> 
+function display(form){ 
+if (form.username.value=="adina") {  
+  if (form.password.value=="123456") {               
+          location="index.php"  
+           } else { 
+             alert("Invalid Password") 
+             } 
+        } else {  alert("Invalid Username") 
+          } 
+      } 
+   </script>    
+<body > 
+<center> 
+ <h1> 
+<form > 
+<input type="text" name="username" /><br><br> 
+<input type="password" name="password"/><br><br> 
+<input type="button" value="Login" onClick="display(this.form)"/> 
+<center> 
+<h1> 
+</form> 
+©️ 2021 GitHub, Inc. 
+Terms
